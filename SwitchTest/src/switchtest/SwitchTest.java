c033a5e28e5846f8707cfa37d3226d0b39633ddd
Acedrin <victor.enaud@hotmail.fr>
2015-11-26 09:12:45 +0100

    Fin de SwitchTest et début des 2 applications

--- SwitchTest/src/switchtest/SwitchTest.java
+++ SwitchTest/src/switchtest/SwitchTest.java
@@ -1,19 +1,27 @@
+package switchtest;
+
 
 public class SwitchTest {
 
     public static void main(String[] args) {
         String color = "red";
         String colorRGB;
-        if (color.equals("black")) {
-            colorRGB = "000000";
-        } else if (color.equals("red")) {
-            colorRGB = "ff0000";
-        } else if (color.equals("green")) {
-            colorRGB = "008000";
-        } else if (color.equals("blue")) {
-            colorRGB = "0000ff";
-        } else {
-            colorRGB = "Invalid  color";
+        switch (color) {
+            case "black":
+                colorRGB = "000000";
+                break;
+            case "red":
+                colorRGB = "ff0000";
+                break;
+            case "green":
+                colorRGB = "008000";
+                break;
+            case "blue":
+                colorRGB = "0000ff";
+                break;
+            default:
+                colorRGB = "Invalid  color";
+                break;
         }
         System.out.println(colorRGB);
     }
